e5bb1edc0102352f38e339006f9b853671346bf3
mark <markpackham1@gmail.com>
2023-08-24 19:26:51 +0100

    add styling
    https://www.youtube.com/watch?v=PAXWRgEo7Ns&list=PL4cUxeGkcC9jZIVqmy_QhfQdi6mzQvJnT&index=6

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "./dojo-logo.png";
+
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="Dojo Logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Dojo Help Desk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
