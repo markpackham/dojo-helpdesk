cb82ca32b833efe593820c5b0f698e3846df3f93
mark <markpackham1@gmail.com>
2023-08-25 14:25:34 +0100

    handle 404 erros
    https://www.youtube.com/watch?v=PbFH_VE1Iks&list=PL4cUxeGkcC9jZIVqmy_QhfQdi6mzQvJnT&index=9

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,3 +1,16 @@
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
 async function getTicket(id) {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
     next: {
@@ -5,6 +18,11 @@ async function getTicket(id) {
       revalidate: 30,
     },
   });
+
+  if (!res.ok) {
+    notFound();
+  }
+
   const data = await res.json();
   return data;
 }
